02c2517b87d9c50312d01b735a91e000c2a6a17c
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-04-02 14:17:39 +0200

    feat: setup Breeze with Bootstrap and create Admin Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        Benvenuto: {{$user->name}}
     </h2>
     <div class="row justify-content-center">
         <div class="col">
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +15,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get("/", [DashboardController::class, 'index'])->name("dashboard");
+    });
+
+
+
+
+
+
+require __DIR__ . '/auth.php';
