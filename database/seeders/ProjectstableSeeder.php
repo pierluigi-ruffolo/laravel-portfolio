70643a2b6ac8afbefe4609b7c42613b4babc9278
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-04-08 11:39:01 +0200

    feat: aggiunto ProjectController e popolamento database con Faker

---
+++ database/seeders/ProjectstableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectstableSeeder extends Seeder
+{
+
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->slug = Str::slug($project->title, '-');
+            $project->client = $faker->company();
+            $project->period = $faker->randomElement(['3 mesi', '2023-2024', 'In corso', '6 mesi']);
+            $project->summary = $faker->paragraph();
+            $project->type = $faker->randomElement(['Front-end', 'Back-end', 'Full-stack', 'Mobile App']);
+            $project->save();
+        }
+    }
+}
