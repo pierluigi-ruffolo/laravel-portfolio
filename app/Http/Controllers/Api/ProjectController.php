4a14ced193a38632c012725fd232dd9c9326de09
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-04-13 12:54:58 +0200

    feat: add index and show views for project display

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+
+    public function index()
+
+    {
+        $projects = Project::with('type')->get();
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show(Project $project)
+    {
+
+        $project->load('technologies');
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\ProjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/projects', [ProjectController::class, 'index']);
+
+
+Route::get('/projects/{project}', [ProjectController::class, 'show']);
